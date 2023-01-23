68daf89348cee64a16f78bc56bb6a592bc488592
James Aules <james_aules@icloud.com>
2023-01-23 15:37:37 -0500

    New places can be added

--- controllers/places.js
+++ controllers/places.js
@@ -1,37 +1,35 @@
 const router = require('express').Router()
-
+const places = require("../models/places")
 
 router.get("/new", (req, res) => {
   res.render("places/new")
 })
 
-router.post("/", (req, res) => {
-  console.log(req.body)
-  res.send("POST /PLACES")
+router.post('/', (req, res) => {
+  
+  //  console.log(req.body)
+  if (!req.body.pic) {
+    // Default image if one is not provided
+    req.body.pic = 'http://placekitten.com/400/400'
+  }
+  if (!req.body.city) {
+    req.body.city = 'Anytown'
+  }
+  if (!req.body.state) {
+    req.body.state = 'USA'
+  }
+  places.push(req.body)
+  res.redirect("/places")
 })
+
 
 
 router.get('/', (req, res) => {
-    //use a work-around to mock up data to use.
-    let places = [{
-
-            name: 'H-Thai-ML',
-            city: 'Seattle',
-            state: 'WA',
-            cuisines: 'Thai, Pan-Asian',
-            pic: '/images/chifa-mateo.png'
-          }, {
-            name: 'Coding Cat Cafe',
-            city: 'Phoenix',
-            state: 'AZ',
-            cuisines: 'Coffee, Bakery',
-            pic: '/images/coding-cat.png'
-          }]
-          
-
     // res.send('GET /places')  //  first route in places.js. We can use the index page (i.e., the GET route that will eventually show a list of all places).
        res.render("places/index", {places})
 })
+
+
 
 
 
---
+++ models/places.js
@@ -0,0 +1,16 @@
+    //use a work-around to mock up data to use.
+    module.exports = [{
+
+        name: 'H-Thai-ML',
+        city: 'Seattle',
+        state: 'WA',
+        cuisines: 'Thai, Pan-Asian',
+        pic: '/images/chifa-mateo.png'
+      }, {
+        name: 'Coding Cat Cafe',
+        city: 'Phoenix',
+        state: 'AZ',
+        cuisines: 'Coffee, Bakery',
+        pic: '/images/coding-cat.png'
+      }]
+      
