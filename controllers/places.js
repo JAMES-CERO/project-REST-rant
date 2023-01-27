20ae0f5d385c340f757580445447f2e11129a6fe
James Aules <james_aules@icloud.com>
2023-01-26 21:30:37 -0500

    added the edit functionality

--- controllers/places.js
+++ controllers/places.js
@@ -58,22 +58,6 @@ router.delete('/:id', (req, res) => {
 
 
 
-
-router.get('/:id/edit', (req, res) => {
-  let id = Number(req.params.id)
-  if (isNaN(id)) {
-      res.render('error404')
-  }
-  else if (!places[id]) {
-      res.render('error404')
-  }
-  else {
-    res.render('places/edit', { place: places[id] })
-  }
-})
-
-
-
 router.put('/:id', (req, res) => {
   
   let id = Number(req.params.id)
@@ -99,6 +83,19 @@ router.put('/:id', (req, res) => {
       // Save the new data into places[id]
       places[id] = req.body
       res.redirect(`/places/${id}`)
+  }
+})
+
+router.get('/:id/edit', (req, res) => {
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+      res.render('error404')
+  }
+  else if (!places[id]) {
+      res.render('error404')
+  }
+  else {
+    res.render('places/edit', { place: places[id], id })
   }
 })
 
