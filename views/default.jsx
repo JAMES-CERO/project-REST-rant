1216a4f19e3283f33d5cbda6854ce55b59b6768f
James Aules <james_aules@icloud.com>
2023-01-28 13:44:02 -0500

    Nav Bar Style & Maxv

--- views/default.jsx
+++ views/default.jsx
@@ -4,21 +4,21 @@ function Def(html) {
     return (
         <html>
             <head>
-                <title> Hello You! </title>
+                <title> James' Rest-Rant </title>
                 <link href="data:image/x-icon;base64,AAABAAEAEBAQAAEABAAoAQAAFgAAACgAAAAQAAAAIAAAAAEABAAAAAAAgAAAAAAAAAAAAAAAEAAAAAAAAAAAAAAA/4QAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAABERERAREREAEREREBEREQAREREQERERABERERAREREAEREREBEREQAREREQERERABERERAAAAAAAAAAAREREQAREREBERERABEREQEREREAERERAREREQAREREBERERABEREQEREREAERERAREREQAAAAAAAAAAD//wAAgIEAAICBAACAgQAAgIEAAICBAACAgQAAgP8AAP8BAACBAQAAgQEAAIEBAACBAQAAgQEAAIEBAAD//wAA" rel="icon" type="image/x-icon" />
                 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" />
                 <link rel="stylesheet" href="/css/style.css" />
             </head>
             <body className="home-menu">
                 <nav>
-                    <ul>
-                        <li>
+                    <ul className="list-inline">
+                        <li className="list-inline-item">
                             <a href="/" >HOME</a>
                         </li>
-                        <li>
+                        <li className="list-inline-item">
                             <a href="/places" >PLACES</a>
                         </li>
-                        <li>
+                        <li className="list-inline-item">
                             <a href="/places/new" >ADD PLACE</a>
                         </li>
                     </ul>
@@ -35,7 +35,7 @@ function Def(html) {
                     <div id="content-wrap">
                         Contact me:
                         <ul>
-                            <li className=""><a href=" ">github</a></li>
+                            <li className=""><a href="https://github.com/JAMES-CERO">github</a></li>
                             <li className=""> <a href=" ">instagram</a></li>
                             <li className=""><a href=" ">phone number</a></li>
                         </ul>
--- views/home.jsx
+++ views/home.jsx
@@ -5,9 +5,10 @@ function home (){
     return (
     <Def>
         <main>
+            <br/>
             <h1>REST-RANT</h1>
             <div>
-                <image src="/images/cereal-bowl.jpg" alt="cereal-bowl picc" ></image>
+                <image id="homeImg" src="/images/cereal-bowl.jpg" alt="cereal-bowl picc" ></image>
             </div>
             <div>
                 Photo By <a href="https://unsplash.com/@alexandermils">Alexander Mils</a> on <a href="https://unsplash.com/photos/5X8oLkzZ1fI">Unsplash</a>
@@ -15,6 +16,7 @@ function home (){
             <a href="/places">
                 <button>Places Page</button>
             </a>
+            <h2> By James Aules </h2>
         </main>
     </Def>
     )
